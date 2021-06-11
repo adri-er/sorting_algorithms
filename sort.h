3a39a8e71257a126b027eee7e651b8f348fcceca
Adriana Echeverri <adriana.er98@hotmail.com>
2021-06-11 10:31:19 -0500

    Create initial files and environment

---
+++ sort.h
@@ -0,0 +1,24 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* -------------------- print.c ---------------------- */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+/* -------------------- STRUCTURES ------------------- */
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+#endif /* SORTH_H */
