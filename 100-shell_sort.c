1d7ef59205cbf8e6990aafcfb7fdde03c56c9246
Adriana Echeverri <adriana.er98@hotmail.com>
2021-06-14 18:11:59 -0500

    [refactor] (shell sort refactored to recursive)

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -22,13 +22,47 @@ void insertion_array(int *array, size_t size)
 		k = i;
 		for (j = k - 1; j >= 0 && array[j] >= array[k];)
 		{
-		SWAP(array[j], array[k], int);
-		j--;
-		k--;
+			SWAP(array[j], array[k], int);
+			j--;
+			k--;
 		}
 	}
 }
-#define IF_PREV_BIGGER(NODE) ((NODE)->prev && ((NODE)->prev->n > (NODE)->n))
+
+/**
+ * insertion_array - insertion algorithm applied to an array.
+ *
+ * @array: array to sort with the algorithm.
+ * @size: size of the array to sort.
+ *
+ * Return: Always void.
+ */
+void insertion_array_gap(int *array, size_t size, int gap)
+{
+	int i;
+
+	if (gap > 1)
+	{
+		i = size - 1;
+		while (i - gap >= 0)
+		{
+			if (array[i] < array[i - gap])
+			{
+				SWAP(array[i], array[i - gap], int);
+			}
+			i--;
+		}
+		print_array((const int *)array, size);
+		gap = (gap - 1) / 3;
+		insertion_array_gap(array, size, gap);
+	}
+	else if (gap == 1)
+	{
+		insertion_array(array, size);
+		print_array((const int *)array, size);
+	}
+}
+
 /**
  * shell_sort - sort an array using shell algorithm.
  *
@@ -40,29 +74,10 @@ void insertion_array(int *array, size_t size)
 void shell_sort(int *array, size_t size)
 {
 	int gap = 1;
-	int i;
 
 	while (gap < ((int)size - 1) / 3)
 	{
 		gap = gap * 3 + 1;
 	}
-	while (gap > 1)
-	{
-		i = size - 1;
-		while (i - gap >= 0)
-		{
-		if (array[i] < array[i - gap])
-		{
-			SWAP(array[i], array[i - gap], int);
-		}
-		i--;
-		}
-		print_array((const int *)array, size);
-		gap = (gap - 1) / 3;
-	}
-	if (gap == 1)
-	{
-		insertion_array(array, size);
-		print_array((const int *)array, size);
-	}
+	insertion_array_gap(array, size, gap);
 }
